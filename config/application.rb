5e205c58cbf59669a357acb131646ef8855b4753
Ubuntu <ubuntu@ip-172-31-13-210.us-east-2.compute.internal>
2020-08-04 12:14:31 +0900

    add ja message

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module ReviewApp
  config.time_zone ='Asia/Tokyo'
  config.active_record.default_timezone = :local 
  config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   end
 end
