5dbdfb7812b6e844f084de2bf7235353a7e48604
Ubuntu <ubuntu@ip-172-31-13-210.us-east-2.compute.internal>
2020-08-05 13:40:15 +0900

    improve facilites of users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+    
+  post "users/:id/update" => "users#update"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
   get 'users/index' => "users#index"
   get 'users/:id' => "users#detail"
+  get 'users/:id/edit' => 'users#edit'
+  
   root 'home#top'
+  
    get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#detail"
@@ -9,4 +16,5 @@ Rails.application.routes.draw do
   get "posts/:id/edit" => "posts#edit"
   post 'posts/:id/update' => 'posts#update'
  post "posts/:id/destroy" => "posts#destroy"
+ 
 end
