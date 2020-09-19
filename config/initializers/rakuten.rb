b1934217ee6532969f1f619268560f0f52f6fc21
Yamashita-Taiki <0809taikiyamashita@gmail.com>
2020-09-19 16:14:09 +0900

    add_gem pry-byebug

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,13 +1,19 @@
-RakutenWebService.configuration do |c|
-  # (Required) Appliction ID for your application.
+RakutenWebService.configure do |c|
+  # (必須) アプリケーションID
   c.application_id = '1051646806531190220'
-  # (Optional) Affiliate ID for your Rakuten account. # default: nil
-  # (Optional) # of retries to send requests when the client receives
-  # When the number of requests in some period overcomes the limit, the endpoints will return
-  # too many requests error. Then the client tries to retry to send the same request after a
-  # while.
-  c.max_retries = 3 # default: 5
-  # (Optional) Enable debug mode. When set true, the client streams out all HTTP requests and
-  # responses to the standard error.
-  c.debug = true # default: false
-  end
+
+<pre><code># (任意) 楽天アフィリエイトID
+c.affiliate_id = '1d0bd364.416f437f.1d0bd365.73d06470' # default: nil
+
+# (任意) リクエストのリトライ回数
+# 一定期間の間のリクエスト数が制限を超えた時、APIはリクエスト過多のエラーを返す。
+# その後、クライアントは少し間を空けた後に同じリクエストを再度送る。
+c.max_retries = 3 # default: 5
+
+# (任意) デバッグモード
+# trueの時、クライアントはすべてのHTTPリクエストとレスポンスを
+# 標準エラー出力に流す。
+c.debug = true # default: false
+</code></pre>
+
+end
