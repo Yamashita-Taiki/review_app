1ca2cdd62586e641fd5df63663348b60a93135be
Yamashita-Taiki <0809taikiyamashita@gmail.com>
2020-09-20 15:57:09 +0900

    rakuten.rbを変更

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -2,18 +2,7 @@ RakutenWebService.configure do |c|
   # (必須) アプリケーションID
   c.application_id = '1051646806531190220'
 
-<pre><code># (任意) 楽天アフィリエイトID
-c.affiliate_id = '1d0bd364.416f437f.1d0bd365.73d06470' # default: nil
-
-# (任意) リクエストのリトライ回数
-# 一定期間の間のリクエスト数が制限を超えた時、APIはリクエスト過多のエラーを返す。
-# その後、クライアントは少し間を空けた後に同じリクエストを再度送る。
-c.max_retries = 3 # default: 5
-
-# (任意) デバッグモード
-# trueの時、クライアントはすべてのHTTPリクエストとレスポンスを
-# 標準エラー出力に流す。
-c.debug = true # default: false
-</code></pre>
+  # (任意) 楽天アフィリエイトID
+  c.affiliate_id = '1d0bd364.416f437f.1d0bd365.73d06470'
 
 end
