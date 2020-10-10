c56029c7dad3c47de38697d504b0165d3d51670a
Yamashita-Taiki <0809taikiyamashita@gmail.com>
2020-10-10 19:02:57 +0900

    search.html.erbでページネーション削除

--- app/controllers/rakuten_controller.rb
+++ app/controllers/rakuten_controller.rb
@@ -7,9 +7,6 @@ class RakutenController < ApplicationController
     hits: 10,
     )
   end
-  @items = Kaminari.paginate_array(body["data"], total_count: meta["total_count"])
-      .page(meta["current_page"])
-      .per(meta["limit_value"])
   end
   
 end
