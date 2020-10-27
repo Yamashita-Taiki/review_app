82c46d416bf1aa8895c5c669e1340c8cb9c50a47
Yamashita-Taiki <0809taikiyamashita@gmail.com>
2020-10-28 01:12:19 +0900

    title,pictureのエラーメッセを日本語化

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,12 @@ class Post < ApplicationRecord
    mount_uploader :picture, PictureUploader
    validates :content, presence: true, length: { maximum: 500 }
    validates :user_id, {presence: true}
+   validates :title, {presence: true}
+   validates :picture, {presence: true}
    has_one_attached :image
 
    def user
       return User.find_by(id: self.user_id)
     end
     
-    # def resized_image
-    #   image.variant(resize_to_limit: [500, 500])
-    # end
 end
