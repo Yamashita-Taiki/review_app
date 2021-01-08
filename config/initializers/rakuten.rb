32d097fcaf1a10115a33d4b28b8b0e3f49ee0afb
Yamashita-Taiki <0809taikiyamashita@gmail.com>
2021-01-08 20:30:10 +0900

    htmlを変更

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,9 +1,2 @@
-RakutenWebService.configure do |c|
-  # (必須) アプリケーションID
-  c.application_id = '1051646806531190220'
-
-  # (任意) 楽天アフィリエイトID
-  c.affiliate_id = '1d0bd364.416f437f.1d0bd365.73d06470'
-
-  
-end
+ENV['c.application_id']
+ENV['c.affiliate_id']
