e93ad25748cb9f4621385cab0ab8a9da18887828
Yamashita-Taiki <0809taikiyamashita@gmail.com>
2021-01-11 20:53:53 +0900

    環境変数を.gitignoreから参照

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,2 +1,8 @@
-ENV['c.application_id']
-ENV['c.affiliate_id']
+RakutenWebService.configure do |c|
+  # (必須) アプリケーションID
+  c.application_id = ENV['APPLICATION_ID']
+
+  # (任意) 楽天アフィリエイトID
+  c.affiliate_id = ENV['AFFILIATE_ID']
+
+end
