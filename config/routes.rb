b924168e0a393e7178c947288699188a3b0877ef
Yamashita-Taiki <0809taikiyamashita@gmail.com>
2021-01-20 14:02:02 +0900

    resources導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,32 @@
 Rails.application.routes.draw do
 
+  resources :users
+  resources :posts
+
   get 'rakuten_search' => 'rakuten#search'
   get "about" => "home#about"
 
-  post "bookmarks/:post_id/create" => "bookmarks#create"
-  post "bookmarks/:post_id/destroy" => "bookmarks#destroy"
-  post "logout" => "users#logout"
-  get  "login" => "users#login_page"
-  post "login" => "users#login"
+  # post "bookmarks/:post_id/create" => "bookmarks#create"
+  # post "bookmarks/:post_id/destroy" => "bookmarks#destroy"
+  # post "logout" => "users#logout"
+  # get  "login" => "users#login_page"
+  # post "login" => "users#login"
 
-  post "users/:id/update" => "users#update"
-  post "users/create" => "users#create"
-  get "signup" => "users#new"
-  get "users/index" => "users#index"
-  get "users/:id" => "users#detail"
-  get "users/:id/edit" => "users#edit"
-  get "users/:id/bookmarks" => "users#bookmarks"
+  # post "users/:id/update" => "users#update"
+  # post "users/create" => "users#create"
+  # get "signup" => "users#new"
+  # get "users/index" => "users#index"
+  # get "users/:id" => "users#detail"
+  # get "users/:id/edit" => "users#edit"
+  # get "users/:id/bookmarks" => "users#bookmarks"
   
    get "/" => "posts#index"
-  get "posts/new" => "posts#new"
-  get "posts/:id" => "posts#detail"
-  post "posts/create" => "posts#create"
-  get "posts/:id/edit" => "posts#edit"
-  post "posts/:id/update" => "posts#update"
- post "posts/:id/destroy" => "posts#destroy"
+#   get "posts/new" => "posts#new"
+#   get "posts/:id" => "posts#detail"
+#   post "posts/create" => "posts#create"
+#   get "posts/:id/edit" => "posts#edit"
+#   post "posts/:id/update" => "posts#update"
+#  post "posts/:id/destroy" => "posts#destroy"
+
  
 end
