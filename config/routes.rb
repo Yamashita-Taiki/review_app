e8811fba353694c391cdc24da870e0933749405f
Yamashita-Taiki <0809taikiyamashita@gmail.com>
2021-02-09 21:22:36 +0900

    rewrite_path

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # post "users/:id/update" => "users#update"
   # post "users/create" => "users#create"
   # get "signup" => "users#new"
-  # get "users/index" => "users#index"
+  # get "users/" => "users#index"
   # get "users/:id" => "users#detail"
   # get "users/:id/edit" => "users#edit"
   get "users/:id/bookmarks" => "users#bookmarks"
