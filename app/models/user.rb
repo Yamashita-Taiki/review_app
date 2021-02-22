fb6aaab682571741e83b7c9b0832647d8773ce3b
Yamashita-Taiki <0809taikiyamashita@gmail.com>
2021-02-22 18:29:46 +0900

    validation順番変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
   has_many :posts
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
-  before_save { self.email = email.downcase }
-  
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },uniqueness: true
+  before_save { self.email = email.downcase }
   validates :image_file, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 
   def my_posts
     return Post.where(user_id: self.id)
