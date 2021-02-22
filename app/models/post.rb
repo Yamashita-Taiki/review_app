91857dba12d0e48d6b77bf97e38f167a053c9e6a
Yamashita-Taiki <0809taikiyamashita@gmail.com>
2021-02-22 18:20:08 +0900

    validationの順番変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
    belongs_to :user
    mount_uploader :picture, PictureUploader
-   validates :content, presence: true, length: { maximum: 500 }
    validates :user_id, {presence: true}
    validates :title, {presence: true}
+   validates :content, presence: true, length: { maximum: 500 }
    validates :picture, {presence: true}
    has_one_attached :image
 
