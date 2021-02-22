7ecd007e0febc3d0b85fa3b8a843f51917cc2d88
Yamashita-Taiki <0809taikiyamashita@gmail.com>
2021-02-22 14:03:03 +0900

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
 
   get 'rakuten_search' => 'rakuten#search'
   get "about" => "home#about"
-
   post "bookmarks/:post_id/create" => "bookmarks#create"
   post "bookmarks/:post_id/destroy" => "bookmarks#destroy"
   post "logout" => "users#logout"
   get  "login" => "users#login_page"
   post "login" => "users#login"
+  get "signup" => "users#new"
+  post "users/create" => "users#create"
   get "users/:id/bookmarks" => "users#bookmarks"
    get "/" => "posts#index"
+  post "posts/create" => "posts#create"
 end
